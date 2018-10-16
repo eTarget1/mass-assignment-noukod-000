89375abe8ef45c2c3b7d029d2838133a32b5e2f4
clifford <clifford@noukod.com>
2018-10-16 14:57:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,9 +5,7 @@ class Person
                 :weight, :handed, :complexion, :t_shirt_size,
                 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each do |key, value|
-      self.send(("#{key}="), value)
-    end
+  d def initialize(attributes)
+    attributes.each {|key, value| self.send("#{key}=",value)}
   end
 end
